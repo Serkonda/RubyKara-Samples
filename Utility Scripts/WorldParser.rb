dcd057a985781539a68a2bb42371e0dd6cf2e718
Serkonda <40118727+Serkonda@users.noreply.github.com>
2019-06-10 19:39:02 +0200

    World Parser 1.1.0 (#2)

--- Utility Scripts/WorldParser.rb
+++ Utility Scripts/WorldParser.rb
@@ -1,29 +1,47 @@
-## World Parser
+## World Parser 
+#   - Version: 1.1.0 - 2019/06/10
 #   - Copyright: https://github.com/serkonda/rubykara-samples/blob/master/LICENSE.md
 
 ## INSTRUCTIONS
 #   - Help: https://github.com/Serkonda/rubykara-samples/wiki/WorldParser.rb
 
-#-- MAIN --#
-# Get the world size
-sizeX = world.getSizeX
-sizeY = world.getSizeY
-worldGenCode = "world.clearAll; world.setSize(#{sizeX}, #{sizeY}); "
 
-# Iterate over world and get world objects
-for x in 0...sizeX
-    for y in 0...sizeY
-        if world.isLeaf(x, y)
-            worldGenCode += "world.setLeaf(#{x}, #{y}, true); "
-        elsif world.isTree(x, y) 
-            worldGenCode += "world.setTree(#{x}, #{y}, true); "        
-        elsif world.isMushroom(x, y)
-            worldGenCode += "world.setMushroom(#{x}, #{y}, true); "
+#-- VARIABLES --#
+$worldCode = ""
+
+#-- METHODS --#
+def parseWorld
+    # Get the world size
+    sizeX = @world.getSizeX
+    sizeY = @world.getSizeY
+    $worldCode = "world.clearAll; world.setSize(#{sizeX}, #{sizeY}); "
+
+    # Iterate over world and get world objects
+    for x in 0...sizeX
+        for y in 0...sizeY
+            if @world.isLeaf(x, y)
+                $worldCode += "world.setLeaf(#{x}, #{y}, true); "
+            end
+            if @world.isTree(x, y) 
+                $worldCode += "world.setTree(#{x}, #{y}, true); "      
+            end  
+            if @world.isMushroom(x, y)
+                $worldCode += "world.setMushroom(#{x}, #{y}, true); "
+            end
         end
     end
 end
 
 # Create a script file and write all code to it
-worldGenFile = File.open("parsedWorld.rb", "w")
-worldGenFile.puts worldGenCode
-worldGenFile.close
+def createFile
+    worldGenFile = File.open("parsedWorld.rb", "w")
+    worldGenFile.puts $worldCode
+    worldGenFile.close
+end
+
+#-- MAIN --#
+parseWorld
+createFile
+
+# Show a success message
+tools.showMessage("World successfully parsed")
---
+++ changelog.md
@@ -0,0 +1,19 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/).
+
+## World Parser
+
+### 1.1.0 - 2019/06/10
+#### Added
+- Message that parsing has finsihed
+
+#### Changed
+- Refactor code to use methods
+
+#### Fixed
+- Mushrooms on a field with a leaf were not parsed
+
+### 1.0.0 - 2019/06/09
+- **Initial Version**
